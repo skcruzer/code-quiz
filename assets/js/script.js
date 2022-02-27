ceab88b74df670e11bc0c584962397b78ec67399
Sean Cruz <seankennethcruz@gmail.com>
2022-02-26 22:23:52 -0800

    added game html, css

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// creating variables for items in page
+const question = document.getElementById('question')
+const question = Array.from(document.getElementById('choice-text'))
+const progressText = document.getElementById('progressText')
+const scoreText = document.getElementById('score')
+const timerEl = document.getElementById('timer')
